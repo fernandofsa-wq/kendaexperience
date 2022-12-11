f2ed27ed3b1b448ce7b8afc1efc64b5c954a16f3
fernandofsa-wq <fernando.araujo.eaa@gmail.com>
2022-12-11 11:33:23 +0000

    2

--- kenda/settings.py
+++ kenda/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%ivqf!%$@%x*scp%+14djca2*mdca1unl&so1b-%a4uod1*-oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
